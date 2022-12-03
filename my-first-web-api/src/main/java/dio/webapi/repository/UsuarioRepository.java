e9516dcb96fd81468814f98ef735b73a94c55775
Renato Marciano <renato_vtr@hotmail.com>
2022-12-03 11:49:20 -0300

    Implementando Spring data jpa Exception Handler

--- my-first-web-api/src/main/java/dio/webapi/repository/UsuarioRepository.java
+++ my-first-web-api/src/main/java/dio/webapi/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package dio.webapi.repository;
 
+import dio.webapi.handler.BusinessException;
+import dio.webapi.handler.CampoObrigatorioException;
 import dio.webapi.model.Usuario;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,12 @@ import java.util.List;
 @Repository
 public class UsuarioRepository {
     public void save(Usuario usuario){
+        if (usuario.getLogin()==null)
+            throw new CampoObrigatorioException("login");
+
+        if (usuario.getPassword()==null)
+            throw new CampoObrigatorioException("password");
+
         if (usuario.getId() == null)
                 System.out.println("SAVE - Recebendo o usuário na camada de repositório");
             else
